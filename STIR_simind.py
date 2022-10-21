daaacfa5708bbcdbeaf5dfa5c09a1b407335d7c3
Sam.Porter <porter.samd@gmail.com>
2022-10-21 11:38:58 +0100

    added STIR python backprojection & image generation

--- STIR_simind.py
+++ STIR_simind.py
@@ -3,13 +3,12 @@
 '''
 
 import os
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
+from pathlib import Path
+current_dir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(current_dir)
 data_file = "template_sinogram.hs"
-data_path = os.path.dirname(os.path.realpath(__file__))
-save_path = data_path
+save_path = current_dir
 attenuation = True
-
-save = False
 
 show_plot = True
 
@@ -21,85 +20,90 @@ import glob
 
 import matplotlib.pyplot as plt
 
-def create_sample_image(image):
-    '''fill the image with some geometric shapes to create a simple phantom image'''
-    im_shape = image.shape()
-    tmp_im = image.get_empty_copy()
-    
-    shapeim = tmp_im.get_empty_copy()
-    
-    # create a body-like ellipsoid shape
-    shape = stir.EllipsoidalCylinder()
-    shape.set_length(400)
-    shape.set_radius_x(im_shape[2]//2*3)
-    shape.set_radius_y(im_shape[1]//3*4)
-    shape.scale = (150)
-    shape.set_origin((stir.FloatCartesianCoordinate3D(0,0,0)))
-    shape.construct_volume(shapeim, stir.IntCartesianCoordinate3D(1,1,1))
-    tmp_im.xapyb(tmp_im,1,shapeim,1)
-
-    # add some lung-like shapes shape
-    shape.set_radius_x(im_shape[2]//2)
-    shape.set_radius_y(im_shape[1])
-    shape.set_origin((stir.FloatCartesianCoordinate3D(0,0,-im_shape[2]//3*2)))
-    shape.construct_volume(shapeim, stir.IntCartesianCoordinate3D(1,1,1))
-    tmp_im.xapyb(tmp_im,1,shapeim,-0.5)
-
-    shape.set_origin((stir.FloatCartesianCoordinate3D(0,0, im_shape[2]//3*2)))
-    shape.construct_volume(shapeim, stir.IntCartesianCoordinate3D(1,1,1))
-    tmp_im.xapyb(tmp_im,1,shapeim,-0.5)
-
-    # a spine-like shape
-    shape.set_radius_x(im_shape[2]//4)
-    shape.set_radius_y(im_shape[1]//4)
-    shape.set_origin((stir.FloatCartesianCoordinate3D(0, im_shape[1]//5*4, 0)))
-    shape.construct_volume(shapeim, stir.IntCartesianCoordinate3D(1,1,1))
-    tmp_im.xapyb(tmp_im,1,shapeim,0.5)
-
-    return tmp_im
-
 def main():
 
-    templ_sino = stir.ProjData.read_from_file(os.path.join(data_path, data_file)) # template sirf acquisition data
+    # firstly let's remove all previous outputs from simind in case this has been run before
+    os.system("rm output*")
 
-    image = stir.FloatVoxelsOnCartesianGrid(templ_sino.get_proj_data_info(), 1)
-    image = stir.zoom_image(create_sample_image(image), 1, 0, 0, 64) # use the template to create an empty image
+    # If the sample image and uMap haven't already been generated, let's do that now
+    image_path = os.path.join(current_dir, "emission_image.smi")
+    if not os.path.exists(image_path):  
+        print("nothing at: " + image_path)
+        print("making files")
+        os.system("sh " + os.path.join(current_dir, "generate_input_data.sh"))
+
+    image = stir.FloatVoxelsOnCartesianGrid.read_from_file(os.path.join(current_dir, "emission_image.hv")) 
 
     if show_plot:
+        # let's have a loook at out ground truth image
         plt.figure()
         plt.subplot(111)
         plt.imshow(np.squeeze(stirextra.to_numpy(image)))
-        plt.title('Simulated Sinogram')
+        plt.title('Ground Truth Image')
         plt.show()
 
-    if attenuation:
-        att_SIMIND = image.get_empty_copy()
-        att_SIMIND.fill((stirextra.to_numpy(image)/0.15).flat) # approximate density image used in SIMIND (mg/cm^3)
-        # att_STIR = image.get_empty_copy()
-        # att_STIR.fill((stirextra.to_numpy(image)/1000).flat) # approximate attenuation coefficient image used in STIR (/cm)
-    
+    # Now we'll add an attenuation image
+    if not attenuation:
+        att_simind = image.get_empty_copy() # zero density map to investigate the effects on SIMIND reconstruction
     else:
-        att_SIMIND = image.get_empty_copy() # zero density map to investigate the effects on SIMIND reconstruction
+        att_STIR = stir.FloatVoxelsOnCartesianGrid.read_from_file(os.path.join(current_dir, "attenuation_image.hv"))
+        att_simind = att_STIR.get_empty_copy()
+        att_simind.fill((stirextra.to_numpy(att_STIR)/0.15*1000).flat) # put our attenuation image in simind's units of density
 
-    if save:
-        image.write_to_file(os.path.join(save_path, "image.smi"))
-        #att_STIR.write_to_file(os.path.join(save_path, "attenuation_stir.hv"))
-        att_SIMIND.write_to_file(os.path.join(save_path, "./attenuation_simind.dmi"))
+    att_simind.write_to_file("attenuation_image_simind.dmi") # and save to file
 
-    os.system("simind input output/NN:0.001/PX:0.4/FS:image.smi/FD:attenuation_simind.dmi")
+    ### now some bash commands ###
+    # The following bash command defines a .smc file `input.smc` follwed by a prefix for output files `output` \
+    # Switches are then used to define:
+    #    * /NN: a multiplier for the number of histories per projection (which is calculated using the sum of all voxel values)
+    #    * /PX: defines the image pixel size in the i,j direction (transverse in this case) - im.voxel_sizes()
+    #    * /FS: defines the prefix for the .smi emission image file
+    #    * /FD: defines the prefix for rhe .dmi attenuation image file
+    
+    os.system("simind input.smc output/NN:0.001/PX:0.4/FS:emission_image.smi/FD:attenuation_image_simind.dmi")
 
-    os.system("sh convertSIMINDToSTIR.sh output.h00")
+    # And (assuming the preious cell ran) we have now simulated our SPECT data!
+    # Next we need to get this data into a format the SIRF will recognise. 
+    # Luckily we have a script ready that does this for us.
+    # This script changes a few lines in the data's header file and the header file suffix. 
+    # Differences between the conventions of interfiles in SIMIND and STIR/SIRF can be found in Rebecca's notes.
 
+    os.system("sh " + os.path.join(current_dir , "convertSIMINDToSTIR.sh") + " output.h00")
+
+    # And we can now create STIR projection data 
     simulated_data =  stir.ProjData.read_from_file("output.hs")
 
     if show_plot:
+        # The sinogram looks like this
         plt.figure()
         plt.subplot(111)
         plt.imshow(np.squeeze(stirextra.to_numpy(simulated_data)))
         plt.title('Simulated Sinogram')
         plt.show()
 
-    return 0
+    ### We now use this simulated data to make a rough reconstruction using back projection
+
+    acq_model_matrix = stir.ProjMatrixByBinSPECTUB() # create a SPECT porjection matrix object
+    acq_model_matrix.set_keep_all_views_in_cache(True) # This keeps views in memory for a speed improvement
+    acq_model_matrix.set_resolution_model(0.1,0.1) # Set a resolution model (just a guess!)
+    if attenuation:
+        acq_model_matrix.set_attenuation_image_sptr(att_STIR) # set our attenuation model in units of cm^{-1}
+    acq_model_matrix.set_up(simulated_data.get_proj_data_info(), image) 
+
+    target = image.get_empty_copy() # create an empty target image (STIR will add on top of non-empty images)
+
+    backprojector = stir.BackProjectorByBinUsingProjMatrixByBin(acq_model_matrix) # create a backprojector object using the matrix above
+    backprojector.set_up(simulated_data.get_proj_data_info(), target)
+
+    backprojector.back_project(target, simulated_data) # And now backproject our data into the target image
+
+    if show_plot:
+        # Let's see what this looks like!
+        plt.figure()
+        plt.subplot(111)
+        plt.imshow(np.squeeze(stirextra.to_numpy(target)))
+        plt.title('Backprojection Image')
+        plt.show()
 
 main()
 
