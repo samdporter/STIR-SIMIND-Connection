20c000ed29e41a349daa791e594c4331bda668ab
Sam.Porter <porter.samd@gmail.com>
2022-11-01 14:07:49 +0000

    add OSEM recon

--- STIR_simind.py
+++ STIR_simind.py
@@ -51,13 +51,12 @@ def main():
         plot_and_save(image, "ground_truth", slice = slice)
 
     # Now we'll add an attenuation image
-    if not attenuation:
-        att_simind = image.get_empty_copy() # zero density map to investigate the effects on SIMIND reconstruction
-    else:
+    if attenuation:
         att_STIR = stir.FloatVoxelsOnCartesianGrid.read_from_file(os.path.join(current_dir, "attenuation_image.hv"))
         att_simind = att_STIR.get_empty_copy()
-        att_simind.fill((stirextra.to_numpy(att_STIR)/0.15*1000).flat) # put our attenuation image in simind's units of density
-
+        att_simind.fill((stirextra.to_numpy(att_STIR)/0.096*1000).flat) # put our attenuation image in simind's units of density
+    else:
+        att_simind = image.get_empty_copy() 
     att_simind.write_to_file("attenuation_image_simind.dmi") # and save to file
 
     ### now some bash commands ###
@@ -68,7 +67,9 @@ def main():
     #    * /FS: defines the prefix for the .smi emission image file
     #    * /FD: defines the prefix for rhe .dmi attenuation image file
     
+
     os.system("simind input.smc output/NN:0.1/PX:0.4/FS:emission_image.smi/FD:attenuation_image_simind.dmi")
+
 
     # And (assuming the preious cell ran) we have now simulated our SPECT data!
     # Next we need to get this data into a format the SIRF will recognise. 
@@ -79,33 +80,74 @@ def main():
     os.system("sh " + os.path.join(current_dir , "convertSIMINDToSTIR.sh") + " output.h00")
 
     # And we can now create STIR projection data 
-    simulated_data =  stir.ProjData.read_from_file("output.hs")
+    simind_projdata =  stir.ProjData.read_from_file("output.hs")
 
     if show_plot:
-        slice = simulated_data.get_num_sinograms()//2
+        slice = simind_projdata.get_num_sinograms()//2
         # The sinogram looks like this
-        plot_and_save(simulated_data, "simulated_data", slice = slice)
+        plot_and_save(simind_projdata, "simind_projdata", slice = slice)
 
     ### We now use this simulated data to make a rough reconstruction using back projection
 
     acq_model_matrix = stir.ProjMatrixByBinSPECTUB() # create a SPECT porjection matrix object
-    acq_model_matrix.set_keep_all_views_in_cache(True) # This keeps views in memory for a speed improvement
+    acq_model_matrix.set_keep_all_views_in_cache(False) # This keeps views in memory for a speed improvement
     acq_model_matrix.set_resolution_model(0.1,0.1) # Set a resolution model (just a guess!)
     if attenuation:
         acq_model_matrix.set_attenuation_image_sptr(att_STIR) # set our attenuation model in units of cm^{-1}
-    acq_model_matrix.set_up(simulated_data.get_proj_data_info(), image) 
+    acq_model_matrix.set_up(simind_projdata.get_proj_data_info(), image) 
 
     target = image.get_empty_copy() # create an empty target image (STIR will add on top of non-empty images)
 
-    backprojector = stir.BackProjectorByBinUsingProjMatrixByBin(acq_model_matrix) # create a backprojector object using the matrix above
-    backprojector.set_up(simulated_data.get_proj_data_info(), target)
+    ### we now want to do something with this data. First we create a STIR forward and back projector pair
+    projector = stir.ProjectorByBinPairUsingProjMatrixByBin(acq_model_matrix)
+    projector.set_up(simind_projdata.get_proj_data_info(), image)
 
-    backprojector.back_project(target, simulated_data) # And now backproject our data into the target image
+    projector.get_back_projector().back_project(target, simind_projdata) # And now backproject our data into the target image
 
     if show_plot:
         # Let's see what this looks like!
         slice = target.get_lengths()[1]//2 # middle slice
         plot_and_save(target, "backprojected_image", slice = slice)
+
+    ### How does simlauted data compare to a STIR forward projection?
+    
+    stir_projdata = stir.ProjDataInMemory(simind_projdata.get_exam_info(),
+                                    simind_projdata.get_proj_data_info())
+    
+    projector.get_forward_projector().forward_project(stir_projdata, image)
+    
+    if show_plot:
+        slice = stir_projdata.get_num_sinograms()//2
+        # The sinogram looks like this
+        plot_and_save(stir_projdata, "stir_projdata", slice = slice)
+        
+    ### Now let's reconstruct this using OSEM ###
+    
+    # create our objective function
+    obj_function = stir.PoissonLogLikelihoodWithLinearModelForMeanAndProjData3DFloat()
+    obj_function.set_proj_data_sptr(simind_projdata)
+    obj_function.set_projector_pair_sptr(projector)
+    
+    # and now our reconstruction object
+    recon = stir.OSMAPOSLReconstruction3DFloat()
+    recon.set_objective_function(obj_function)
+    recon.set_num_subsets(2)
+    recon.set_num_subiterations(4)
+    recon.set_max_num_full_iterations(4)
+    
+    # create a dummy image to fill with out reconstructed image
+    target = image.get_empty_copy()
+    target.fill(1)
+
+    # and reconstruct
+    recon.set_up(target)
+    s = recon.reconstruct(target)
+    print(s) # This will tell us if the recontruction has been helpful
+    
+    if show_plot:
+        slice = stir_projdata.get_num_sinograms()//2
+    # The recpnstructed image
+    plot_and_save((target), "reconstructed_image", slice = slice)
 
 main()
 
