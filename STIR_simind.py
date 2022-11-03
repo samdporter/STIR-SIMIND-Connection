c841e373e0f658d0833935df17efdc751ca17190
Sam.Porter <porter.samd@gmail.com>
2022-11-03 12:28:15 +0000

    updated notebook adn script for STIR

--- STIR_simind.py
+++ STIR_simind.py
@@ -1,16 +1,25 @@
 
 '''STIR - SIMIND connection demo.
+
+Usage:
+  STIR_simind [--help | options]
+  
+Options:
+  -f <file>, --file=<file>              raw data file [default: template_sinogram.hs]
+  -p <path>, --path=<path>              path to data files, defaults to current directory
+  -s <save>, --save=<save>              path to save files, defaults to current directory
+  -a <attn>, --attenuation=<attn>       whether to use attenuation in reconstruction & simulation. True or False.  [default: True]
+  --non-interactive                     do not show plots
 '''
 
+__version__ = '0.0.1'
+
+from docopt import docopt
+
 import os
-from pathlib import Path
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(current_dir)
-data_file = "template_sinogram.hs"
-save_path = current_dir
-attenuation = True
-
-show_plot = True
 
 import stir
 import stirextra
@@ -20,7 +29,7 @@ import glob
 
 import matplotlib.pyplot as plt
 
-def plot_and_save(image, fname, slice = False):
+def plot_show_save(image, fname, slice = False):
         ''' simple image plot and save function '''
         plt.subplot(111)
         if slice:
@@ -30,8 +39,34 @@ def plot_and_save(image, fname, slice = False):
         plt.title(fname)
         plt.savefig(fname)
         plt.show()   
+        image.write_to_file('tmp_'+fname)
 
-def main():
+def to_projdata(numpy_array, proj_data_info, exam_info):
+    ''' get STIR projection data from numpy array'''
+    projdataout=stir.ProjDataInMemory(exam_info, proj_data_info)
+    projdataout.fill(numpy_array.flat)
+    return projdataout
+
+def main(args):
+    
+    data_file = args['--file']
+    if data_file is None:
+        data_file = "template_sinogram.hs"
+
+    data_path = args['--path']
+    if data_path is None:
+        data_path = current_dir
+
+    save_path = args['--save']
+    if save_path is None:
+        save_path = current_dir
+
+    if args['--attenuation'] == 'False':
+        attenuation = False
+    else:
+        attenuation = True
+
+    show_plot = not args['--non-interactive']
 
     # firstly let's remove all previous outputs from simind in case this has been run before
     os.system("rm output*")
@@ -48,7 +83,7 @@ def main():
     if show_plot:
         # let's have a loook at out ground truth image
         slice = image.get_lengths()[1]//2 # middle slice
-        plot_and_save(image, "ground_truth", slice = slice)
+        plot_show_save(image, "ground_truth", slice = slice)
 
     # Now we'll add an attenuation image
     if attenuation:
@@ -58,6 +93,11 @@ def main():
     else:
         att_simind = image.get_empty_copy() 
     att_simind.write_to_file("attenuation_image_simind.dmi") # and save to file
+    
+    if show_plot:
+        slice = att_simind.get_lengths()[1]//2 # middle slice
+        # The sinogram looks like this
+        plot_show_save(att_simind, "attenuation_image", slice = slice)
 
     ### now some bash commands ###
     # The following bash command defines a .smc file `input.smc` follwed by a prefix for output files `output` \
@@ -68,7 +108,7 @@ def main():
     #    * /FD: defines the prefix for rhe .dmi attenuation image file
     
 
-    os.system("simind input.smc output/NN:0.1/PX:0.4/FS:emission_image.smi/FD:attenuation_image_simind.dmi")
+    os.system("simind input.smc output/NN:1/PX:0.4/FS:emission_image.smi/FD:attenuation_image_simind.dmi")
 
 
     # And (assuming the preious cell ran) we have now simulated our SPECT data!
@@ -77,15 +117,19 @@ def main():
     # This script changes a few lines in the data's header file and the header file suffix. 
     # Differences between the conventions of interfiles in SIMIND and STIR/SIRF can be found in Rebecca's notes.
 
-    os.system("sh " + os.path.join(current_dir , "convertSIMINDToSTIR.sh") + " output.h00")
+    os.system("sh " + os.path.join(current_dir , "convertSIMINDToSTIR.sh") + " output_tot_w1.h00")
 
     # And we can now create STIR projection data 
-    simind_projdata =  stir.ProjData.read_from_file("output.hs")
+    simind_projdata_flipped =  stir.ProjData.read_from_file("output_tot_w1.hs")
+    projdata_arr = stirextra.to_numpy(simind_projdata_flipped)
+
+    simind_projdata = to_projdata(projdata_arr, simind_projdata_flipped.get_proj_data_info(),
+                simind_projdata_flipped.get_exam_info())
 
     if show_plot:
         slice = simind_projdata.get_num_sinograms()//2
         # The sinogram looks like this
-        plot_and_save(simind_projdata, "simind_projdata", slice = slice)
+        plot_show_save(simind_projdata, "simind_projdata", slice = slice)
 
     ### We now use this simulated data to make a rough reconstruction using back projection
 
@@ -102,13 +146,6 @@ def main():
     projector = stir.ProjectorByBinPairUsingProjMatrixByBin(acq_model_matrix)
     projector.set_up(simind_projdata.get_proj_data_info(), image)
 
-    projector.get_back_projector().back_project(target, simind_projdata) # And now backproject our data into the target image
-
-    if show_plot:
-        # Let's see what this looks like!
-        slice = target.get_lengths()[1]//2 # middle slice
-        plot_and_save(target, "backprojected_image", slice = slice)
-
     ### How does simlauted data compare to a STIR forward projection?
     
     stir_projdata = stir.ProjDataInMemory(simind_projdata.get_exam_info(),
@@ -119,7 +156,7 @@ def main():
     if show_plot:
         slice = stir_projdata.get_num_sinograms()//2
         # The sinogram looks like this
-        plot_and_save(stir_projdata, "stir_projdata", slice = slice)
+        plot_show_save(stir_projdata, "stir_projdata", slice = slice)
         
     ### Now let's reconstruct this using OSEM ###
     
@@ -131,9 +168,9 @@ def main():
     # and now our reconstruction object
     recon = stir.OSMAPOSLReconstruction3DFloat()
     recon.set_objective_function(obj_function)
-    recon.set_num_subsets(2)
-    recon.set_num_subiterations(4)
-    recon.set_max_num_full_iterations(4)
+    recon.set_num_subsets(9)
+    recon.set_num_subiterations(9)
+    recon.set_max_num_full_iterations(1)
     
     # create a dummy image to fill with out reconstructed image
     target = image.get_empty_copy()
@@ -147,10 +184,11 @@ def main():
     if show_plot:
         slice = stir_projdata.get_num_sinograms()//2
     # The recpnstructed image
-    plot_and_save((target), "reconstructed_image", slice = slice)
-
-main()
-
+    plot_show_save((target), "reconstructed_image", slice = slice)
+    
+args = docopt(__doc__, version=__version__)
+main(args)
+    
 for f in glob.glob(("./tmp*")):
     os.remove(f)
 
